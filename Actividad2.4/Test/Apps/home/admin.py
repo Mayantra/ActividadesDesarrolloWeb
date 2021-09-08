b9f1eb78ac1033f9fed16a54ad705e8b44df9273
Mayantra <isaac.vassquez@gmial.com>
2021-09-07 19:13:45 -0600

    modelos Estudiantes

--- Actividad2.4/Test/Apps/home/admin.py
+++ Actividad2.4/Test/Apps/home/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Cliente, TipoCliente, Ventas
+from .models import Administrador, Articulo, Comentario, Estudiante  
 
 # Register your models here.
-admin.site.register(Cliente)
-admin.site.register(TipoCliente)
-admin.site.register(Ventas)
+admin.site.register(Estudiante)
+admin.site.register(Administrador)
+admin.site.register(Articulo)
+admin.site.register(Comentario)
